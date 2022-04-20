e5ad6aa8b59dbcf5adc45c67ce736e3996803bde
Andy SHU <andy091045@gmail.com>
2022-04-21 02:30:51 +0800

    3 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// @Component({
+//     selector: 'app-item',
+//     templateUrl: './item/item.component.html',
+//     styleUrls: ['./item/item.component.css']
+//   })
 export class AppComponent {
   title = 'firstAngular';
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  addItem(description: string) {
+    //unshift會在開頭增加新的元素
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item: any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
 }
